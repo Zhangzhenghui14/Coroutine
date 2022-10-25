4dd2770e61f05a92a884acaee73a1517e0dd3659
Zhangzhenghui14 <73372852+Zhangzhenghui14@users.noreply.github.com>
2022-10-25 17:23:20 +0800

    Create ZNull.cs
     等待一次循环（每次循环的时间间隔为一帧，所以也可以说是等待一帧）
     相等于unity协程中的yield return null

---
+++ Assets/Scripts/ZNull.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// 等待一次循环（每次循环的时间间隔为一帧，所以也可以说是等待一帧）
+/// 相等于unity协程中的yield return null
+/// </summary>
+public class ZNull : IWait
+{
+	public bool Tick()
+	{
+		return true;
+	}
+}
